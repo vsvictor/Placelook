44246a557ae780ecb3f1d11e326bd044d5cc4f3c
Віктор Дмитрович Джурляк <viktor.dzhurliak@infotech.gov.ua>
2024-10-14 19:22:15 +0300

    Hive local

--- lib/model/languages.dart
+++ lib/model/languages.dart
@@ -1,8 +1,27 @@
+import 'package:hive/hive.dart';
+part 'languages.g.dart';
+
+@HiveType(typeId: 8)
+class LanguagesPresent extends HiveObject {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final Languages language;
+  LanguagesPresent({required this.id, required this.language});
+}
+
+@HiveType(typeId: 9)
 enum Languages {
+  @HiveField(0)
   ENGLISH("English"),
+  @HiveField(1)
   FRANCH("Franch"),
+  @HiveField(2)
   GERMAN("German"),
+  @HiveField(3)
   UKRANIAN("Ukranian"),
+  @HiveField(4)
   UNDEFINED("Undefined");
 
   final String title;
